b4f2654a27d49b16da4ca217c097916a6fd4f8d9
HEEL PATEL <heelpatel216@gmail.com>
2024-10-15 19:38:55 +0530

    Update AxiosInstance baseURL for production deployment

--- Frontend/src/Utils/AxiosInstance.js
+++ Frontend/src/Utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/", //   Replace for Hosting development:- import.meta.env.VITE_BASE_URL",
+  baseURL: "https://new-crypto-compass-production.up.railway.app/", //   Replace for Hosting development:- import.meta.env.VITE_BASE_URL",
   withCredentials: true,
 });
 
